9375ecf89b88def0f92b6d46a0aef3dab9d77a8b
Mirko Nasato <mirko@nasato.dev>
2021-07-10 09:00:00 +0000

    Shared Page Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
 import React from 'react';
+import Page from '../components/Page';
 import ProductCard from '../components/ProductCard';
-import Title from '../components/Title';
 import { getProducts, Product } from '../lib/products';
 
 interface HomePageProps {
@@ -22,21 +20,15 @@ export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
 const HomePage: React.FC<HomePageProps> = ({ products }) => {
   console.log('[HomePage] render:', products);
   return (
-    <>
-      <Head>
-        <title>Next Shop</title>
-      </Head>
-      <main className="px-6 py-4">
-        <Title>Next Shop</Title>
-        <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          {products.map((product) => (
-            <li key={product.id}>
-              <ProductCard product={product} />
-            </li>
-          ))}
-        </ul>
-      </main>
-    </>
+    <Page title="Indoor Plants">
+      <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        {products.map((product) => (
+          <li key={product.id}>
+            <ProductCard product={product} />
+          </li>
+        ))}
+      </ul>
+    </Page>
   );
 };
 
---
+++ pages/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from '../components/Page';
+
+const SignInPage: React.FC = () => {
+  return (
+    <Page title="Sign In">
+
+    </Page>
+  );
+};
+
+export default SignInPage;
