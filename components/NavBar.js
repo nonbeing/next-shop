8a185fbfcab3de446aa0b4b8f03d735cf73c0ed9
Mirko Nasato <mirko@nasato.dev>
2021-07-10 09:00:00 +0000

    NavBar with Authentication

---
+++ components/NavBar.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+function NavBar() {
+  const user = undefined;
+  return (
+    <nav className="px-2 py-1 text-sm">
+      <ul className="flex gap-2">
+        <li className="text-lg font-extrabold">
+          <Link href="/">
+            <a>
+              Next Shop
+            </a>
+          </Link>
+        </li>
+        <li role="separator" className="flex-1" />
+        {user ? (
+          <>
+            <li>
+              {user.name}
+            </li>
+            <li>
+              <button>Sign Out</button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link href="/sign-in">
+              <a>
+                Sign In
+              </a>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
