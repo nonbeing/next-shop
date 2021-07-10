61ba2d29cda50bf3d95a0cbf61f375dc176275dc
Mirko Nasato <mirko@nasato.dev>
2021-07-10 09:00:00 +0000

    Sign In Form

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Button from '../components/Button';
+import Field from '../components/Field';
+import Input from '../components/Input';
 import Page from '../components/Page';
 
 const SignInPage: React.FC = () => {
   return (
     <Page title="Sign In">
-
+      <form>
+        <Field label="Email">
+          <Input type="email" />
+        </Field>
+        <Field label="Password">
+          <Input type="password" />
+        </Field>
+        <Button type="submit">
+          Sign In
+        </Button>
+      </form>
     </Page>
   );
 };
