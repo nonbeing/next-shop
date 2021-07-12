ac3c453bc72e528235e4049d17b949981561f9dd
Mirko Nasato <mirko@nasato.dev>
2021-07-12 09:00:00 +0000

    Cart Page

---
+++ pages/cart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from '../components/Page';
+
+const CartPage: React.FC = () => {
+  return (
+    <Page title="Cart">
+
+    </Page>
+  );
+};
+
+export default CartPage;
