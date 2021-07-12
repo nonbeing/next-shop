502d3577aa66f9d1b6370e6679b0acd4be08a3fd
Mirko Nasato <mirko@nasato.dev>
2021-07-12 09:00:00 +0000

    Fetching Cart Items

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import Page from '../components/Page';
+import { fetchJson } from '../lib/api';
+import { CartItem } from '../lib/cart';
 
 const CartPage: React.FC = () => {
+  const query = useQuery<CartItem[]>('cartItems', () => fetchJson('/api/cart'));
+  const cartItems = query.data;
+
+  console.log('[CartPage] cartItems:', cartItems);
   return (
     <Page title="Cart">
 
